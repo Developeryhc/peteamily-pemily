bf91b9143f385e0cb41e5582ecd82a78dc7a3b16
sungyeji <sungyeji@DESKTOP-TE3SEB2>
2021-05-20 18:04:01 +0900

    1. 나 성예지 잘했음

--- pemilySemiPjt/src/care/model/controller/CareDeleteServlet.java
+++ pemilySemiPjt/src/care/model/controller/CareDeleteServlet.java
@@ -1,11 +1,15 @@
 package care.model.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import care.model.service.CareService;
 
 /**
  * Servlet implementation class CareDeleteServlet
@@ -26,8 +30,18 @@ public class CareDeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		int careNo = Integer.parseInt(request.getParameter("careNo"));
+		int result = new CareService().deleteCare(careNo);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+		if(result>0) {
+			request.setAttribute("msg", "삭제성공");
+			request.setAttribute("loc", "/careList?reqPage=1");
+		}else {
+			request.setAttribute("msg", "삭제실패");
+			request.setAttribute("loc", "/careView?careNo"+careNo);
+		}
+		rd.forward(request, response);
 	}
 
 	/**
